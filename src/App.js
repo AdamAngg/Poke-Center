fdb74ec321e428a4701dd32c6efebc69862a3576
AdamAngg <adam.nglart@gmail.com>
2023-04-11 23:34:24 +0200

    Added scss + set styles folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import "./App.css";
+import React from "react";
+import "./styles/styles.scss";
 import pokemon from "./pokemon.json";
 
 const PokemnonRow = ({ pokemon }) => (
@@ -7,25 +8,34 @@ const PokemnonRow = ({ pokemon }) => (
     <td>{pokemon.type.join("/")}</td>
   </tr>
 );
+const RowTitle = () => (
+  <thead>
+    <tr>
+      <th>Pokemon</th>
+      <th>Type</th>
+    </tr>
+  </thead>
+);
 
 function App() {
+  const [filter, filterSet] = React.useState("");
   return (
     <div style={{ margin: "auto", width: 800, padding: "1rem" }}>
       <h1 className="title">Pokemon search</h1>
-      <table width={"100%"}>
-        <thead>
-          <tr>
-            <th>Pokemon</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pokemon.slice(0, 20).map((pokemon) => (
-            <PokemnonRow pokemon={pokemon} key={pokemon.id} />
-          ))}
-        </tbody>
-      </table>
-      ;
+      <input value={filter} onChange={(evt) => filterSet(evt.target.value)} />
+      <div>
+        <table width={"100%"}>
+          <RowTitle />
+          <tbody>
+            {pokemon
+              .filter((pokemon) => pokemon.name.english.includes(filter))
+              .slice(0, 20)
+              .map((pokemon) => (
+                <PokemnonRow pokemon={pokemon} key={pokemon.id} />
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
