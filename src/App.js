6d8eea1ef710be4fa5121bdb60551e7a4c1cef72
AdamAngg <adam.nglart@gmail.com>
2023-04-12 15:56:59 +0200

    Added more styles & fetched data from public folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles/styles.scss";
-import pokemon from "./pokemon.json";
+import LoadingSpinner from "./Components/Spinner";
 
-const PokemnonRow = ({ pokemon }) => (
+const PokemnonRow = ({ pokemon, onSelect }) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join("/")}</td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>Select</button>
+    </td>
   </tr>
 );
 const RowTitle = () => (
@@ -16,25 +19,58 @@ const RowTitle = () => (
     </tr>
   </thead>
 );
-
+const PokemonInfo = ({ name: { english }, base }) => (
+  <div>
+    <h1>{english}</h1>
+    {Object.keys(base).map((e) => (
+      <p key={e}>
+        {e}: {base[e]}
+      </p>
+    ))}
+  </div>
+);
+const spinner = () => (
+  <div className="spinner-container">
+    <div className="loading-spinner"></div>
+  </div>
+);
 function App() {
   const [filter, filterSet] = React.useState("");
+  const [pokemon, pokemonSet] = React.useState([]);
+  const [selectedItem, selectedItemSet] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/pokemon.json")
+      .then((resp) => resp.json())
+      .then((data) => pokemonSet(data));
+  }, []);
+
   return (
-    <div style={{ margin: "auto", width: 800, padding: "1rem" }}>
+    <div className="section">
       <h1 className="title">Pokemon search</h1>
       <input value={filter} onChange={(evt) => filterSet(evt.target.value)} />
-      <div>
-        <table width={"100%"}>
-          <RowTitle />
-          <tbody>
-            {pokemon
-              .filter((pokemon) => pokemon.name.english.includes(filter))
-              .slice(0, 20)
-              .map((pokemon) => (
-                <PokemnonRow pokemon={pokemon} key={pokemon.id} />
-              ))}
-          </tbody>
-        </table>
+
+      <div className="grid">
+        <div>
+          <table width={"100%"}>
+            <RowTitle />
+            <tbody>
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english.toLowerCase().includes(filter)
+                )
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemnonRow
+                    pokemon={pokemon}
+                    key={pokemon.id}
+                    onSelect={(pokemon) => selectedItemSet(pokemon)}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
       </div>
     </div>
   );
