84e35a773d515b3234e6803154519a19ce0c9799
AdamAngg <adam.nglart@gmail.com>
2023-05-19 23:49:17 +0200

    chore: move object with links to seperate file

---
+++ src/Components/Generations/Generations.data.js
@@ -0,0 +1,6 @@
+export const links = {
+  Generation1: "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
+  Generation2: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=151",
+  Generation3: "https://pokeapi.co/api/v2/pokemon?limit=135&offset=251",
+  Generation4: "https://pokeapi.co/api/v2/pokemon?limit=108&offset=386",
+};
--- src/Components/Generations/Generations.jsx
+++ src/Components/Generations/Generations.jsx
@@ -1,14 +1,9 @@
 import { GenerationButton } from "./GenerationButton";
+import { links } from "./Generations.data";
 export const Generations = () => {
-  const pokemonGenerationsUrl = {
-    Generation1: "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
-    Generation2: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=151",
-    Generation3: "https://pokeapi.co/api/v2/pokemon?limit=135&offset=251",
-    Generation4: "https://pokeapi.co/api/v2/pokemon?limit=108&offset=386",
-  };
   return (
     <div className="generations-container">
-      {Object.entries(pokemonGenerationsUrl).map(([key, value], id) => (
+      {Object.entries(links).map(([key, value], id) => (
         <GenerationButton key={key} name={key} value={value} id={id + 1} />
       ))}
     </div>
